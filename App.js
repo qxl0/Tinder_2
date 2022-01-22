054168447ef1b9ec82845d3d671f3d09b17e956b
qiang li <qxl0@yahoo.com>
2022-01-22 18:00:30 -0500

    Reanimated works

--- App.js
+++ App.js
@@ -7,14 +7,32 @@ import {
   StyleSheet,
   Button,
   ImageBackground,
+  Pressable,
 } from 'react-native';
 import TinderCard from './src/components/TinderCard';
 import users from './assets/data/users';
+import Animated, {
+  useAnimatedStyle,
+  useSharedValue,
+  withSpring,
+} from 'react-native-reanimated';
 
 const App = () => {
+  const sharedValue = useSharedValue(1);
+
+  const cardStyle = useAnimatedStyle(() => ({
+    opacity: sharedValue.value,
+  }));
+
   return (
     <View style={styles.container}>
-      <TinderCard user={users[3]} />
+      <Animated.View style={[styles.animatedCard, cardStyle]}>
+        <TinderCard user={users[3]} />
+      </Animated.View>
+      <Pressable
+        onPress={() => (sharedValue.value = withSpring(Math.random()))}>
+        <Text>Change Value</Text>
+      </Pressable>
     </View>
   );
 };
@@ -22,9 +40,14 @@ const App = () => {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
+  },
+  animatedCard: {
+    width: '95%',
+    height: '90%',
+    justifyContent: 'center',
+    alignItems: 'center',
   },
 });
 
